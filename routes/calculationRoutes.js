e57dcdae7264569259d5d7683d579edbf497ec01
AgriDose <biolutte.ent@gmail.com>
2025-07-04 20:44:32 +0100

    calculationRoutes.js

--- routes/calculationRoutes.js
+++ routes/calculationRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const { calculateRecommendations } = require('../controllers/calculationController');
 
+// POST /api/calculate
 router.post('/', calculateRecommendations);
+
+// GET /api/calculate/test (لأغراض الاختبار)
+router.get('/test', (req, res) => {
+  res.json({ message: 'نظام الحسابات يعمل بشكل صحيح!' });
+});
 
 module.exports = router;
