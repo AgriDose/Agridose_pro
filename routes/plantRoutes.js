5a74238ac760515ca845e6ad8eb28eca6e666c1d
AgriDose <biolutte.ent@gmail.com>
2025-07-05 00:23:44 +0100

    plantRoutes.js

--- routes/plantRoutes.js
+++ routes/plantRoutes.js
@@ -1,22 +1,14 @@
 const express = require('express');
 const {
-  getPlants,
-  getPlant,
-  createPlant,
-  updatePlant,
-  deletePlant
+  getPlantTypes,
+  getPlantsByType,
+  getPlantDetails
 } = require('../controllers/plantController');
-const { protect, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.route('/')
-  .get(getPlants)
-  .post(protect, authorize('admin'), createPlant);
-
-router.route('/:id')
-  .get(getPlant)
-  .put(protect, authorize('admin'), updatePlant)
-  .delete(protect, authorize('admin'), deletePlant);
+router.get('/types', getPlantTypes);
+router.get('/:type', getPlantsByType);
+router.get('/details/:id', getPlantDetails);
 
 module.exports = router;
