344edcbb54a1618a87e26b596c08fa3bfbaa8f7e
AgriDose <biolutte.ent@gmail.com>
2025-07-05 01:12:55 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000
     });
-    console.log('تم الاتصال بقاعدة البيانات بنجاح');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (err) {
-    console.error('فشل الاتصال بقاعدة البيانات:', err.message);
+    console.error(`Error: ${err.message}`);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
