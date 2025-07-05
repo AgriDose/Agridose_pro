dd288b3c12e480de076630ac9ff1baa5e4f8e95e
AgriDose <biolutte.ent@gmail.com>
2025-07-05 14:33:11 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,10 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
     console.log('✅ تم الاتصال بقاعدة البيانات');
   } catch (err) {
