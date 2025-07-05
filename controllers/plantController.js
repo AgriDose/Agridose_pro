fba490b8acf250a8bb31f611ad1111489f155349
AgriDose <biolutte.ent@gmail.com>
2025-07-05 16:02:18 +0100

    plantController.js

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -1,60 +1,40 @@
 const Plant = require('../models/Plant');
 const asyncHandler = require('../middleware/asyncHandler');
+const ErrorResponse = require('../utils/ErrorResponse');
 
 // @desc    الحصول على جميع أنواع النباتات
 // @route   GET /api/plants/types
 // @access  Public
-exports.getPlantTypes = asyncHandler(async (req, res) => {
-  const plants = await Plant.find({});
-  const types = [...new Set(plants.map(p => p.type.ar))];
-  
-  res.status(200).json({
-    success: true,
-    data: types
-  });
+exports.getPlantTypes = asyncHandler(async (req, res, next) => {
+  const types = await Plant.distinct('type');
+  res.status(200).json({ success: true, data: types });
 });
 
 // @desc    الحصول على الأصناف حسب النوع
-// @route   GET /api/plants/:type
+// @route   GET /api/plants/varieties/:type
 // @access  Public
-exports.getPlantsByType = asyncHandler(async (req, res) => {
-  const type = req.params.type;
+exports.getPlantVarieties = asyncHandler(async (req, res, next) => {
+  const { type } = req.params;
   
-  const plants = await Plant.find({});
-  const filteredPlants = plants.filter(p => p.type.ar === type);
-  
-  if (!filteredPlants.length) {
-    return res.status(404).json({
-      success: false,
-      message: 'لم يتم العثور على نباتات من هذا النوع'
-    });
+  if (!['خضر', 'حبوب', 'أشجار مثمرة'].includes(type)) {
+    return next(new ErrorResponse('Invalid plant type', 400));
   }
-  
-  res.status(200).json({
-    success: true,
-    data: filteredPlants.map(p => ({
-      id: p._id,
-      name: p.name,
-      scientific_name: p.scientific_name
-    }))
-  });
+
+  const varieties = await Plant.distinct('variety', { type });
+  res.status(200).json({ success: true, data: varieties });
 });
 
-// @desc    الحصول على تفاصيل نبتة محددة
-// @route   GET /api/plants/details/:id
+// @desc    الحصول على تفاصيل صنف معين
+// @route   GET /api/plants/details/:type/:variety
 // @access  Public
-exports.getPlantDetails = asyncHandler(async (req, res) => {
-  const plant = await Plant.findById(req.params.id);
+exports.getPlantDetails = asyncHandler(async (req, res, next) => {
+  const { type, variety } = req.params;
+
+  const plant = await Plant.findOne({ type, variety });
   
   if (!plant) {
-    return res.status(404).json({
-      success: false,
-      message: 'لم يتم العثور على النبتة'
-    });
+    return next(new ErrorResponse('Plant variety not found', 404));
   }
-  
-  res.status(200).json({
-    success: true,
-    data: plant
-  });
+
+  res.status(200).json({ success: true, data: plant });
 });
