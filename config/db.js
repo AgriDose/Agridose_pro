7f951acb5598da35e74ce830d9ba16670cb3b0ef
AgriDose <biolutte.ent@gmail.com>
2025-07-05 17:13:41 +0100

    db.js

--- config/db.js
+++ config/db.js
@@ -2,16 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('MongoDB Connected');
   } catch (err) {
-    console.error(`Database connection error: ${err.message}`);
+    console.error('Connection failed:', err.message);
     process.exit(1);
   }
 };
