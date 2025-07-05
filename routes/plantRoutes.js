beb03833c4893ff2045bc430e29b35c2f734b965
AgriDose <biolutte.ent@gmail.com>
2025-07-05 02:18:59 +0100

    plantRoutes.js

--- routes/plantRoutes.js
+++ routes/plantRoutes.js
@@ -2,10 +2,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ 
-    message: 'قائمة النباتات',
-    data: []
-  });
+  res.json({ message: 'Plants endpoint' });
 });
 
-export { router as plantRoutes };
+export default router;  // التصدير بهذه الطريقة
