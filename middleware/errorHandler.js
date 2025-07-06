540da1bb5d5602eb8181bfdcb29cba263cbfe5b2
AgriDose <biolutte.ent@gmail.com>
2025-07-06 01:39:26 +0100

    errorHandler.js

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,19 +4,19 @@ const errorHandler = (err, req, res, next) => {
   let error = { ...err };
   error.message = err.message;
 
-  // خطأ Mongoose bad ObjectId
+  // Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Resource not found with id of ${err.value}`;
+    const message = 'Resource not found';
     error = new ErrorResponse(message, 404);
   }
 
-  // خطأ تكرار في Mongoose
+  // Mongoose duplicate key
   if (err.code === 11000) {
     const message = 'Duplicate field value entered';
     error = new ErrorResponse(message, 400);
   }
 
-  // خطأ تحقق Mongoose
+  // Mongoose validation error
   if (err.name === 'ValidationError') {
     const message = Object.values(err.errors).map(val => val.message);
     error = new ErrorResponse(message, 400);
